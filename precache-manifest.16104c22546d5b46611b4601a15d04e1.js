10bc6a59001ba7e952560f8737846e509f34e54b
hg <hgson.8@gmail.com>
2020-07-28 01:11:07 +0900

    Updates

---
+++ precache-manifest.16104c22546d5b46611b4601a15d04e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c296bdb036552531871b9e4a2a3890a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9ddcc075185d9cd08d36",
+    "url": "/movie_app_2020/static/css/main.0e46132c.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app_2020/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddcc075185d9cd08d36",
+    "url": "/movie_app_2020/static/js/main.8e067b71.chunk.js"
+  },
+  {
+    "revision": "4839ee59ec104f2d85b3",
+    "url": "/movie_app_2020/static/js/runtime-main.2f3c173d.js"
+  }
+]);
